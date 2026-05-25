da01a9bfdad8b433089d333f577fc9e8107c4589
karishma-fariha <karishmafarihakathi10@gmail.com>
2026-05-25 23:01:31 +0600

    added api route

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,54 @@
 import http, { IncomingMessage, Server, ServerResponse } from "http";
 import config from "./config";
-const server: Server = http.createServer(
-    (req: IncomingMessage, res: ServerResponse) => {
-        console.log("server is running....");
-        // root route
-        if (req.url == "/" && req.method == "GET") {
-            res.writeHead(200, { "content-type": "application/json" });
+import addRoutes, { routes } from "./helper/RouteHandler";
+
+addRoutes("GET","/",(req,res)=>{
+  res.writeHead(200, { "content-type": "application/json" });
             res.end(
                 JSON.stringify({
                     message: "Hello from node js with typescript...",
                     path: req.url,
                 })
-            )
+            )  
+})
+const server: Server = http.createServer(
+    (req: IncomingMessage, res: ServerResponse) => {
+        console.log("server is running....");
+
+        const method = req.method?.toUpperCase()||"";
+        const path = req.url||"";
+        const methodMap = routes.get(method);
+        const handler = methodMap?.get (path)
+
+        if(handler){
+
+        }else{
+            res.writeHead(404,{"content-type":"application/json"});
         }
+
+
+        // root route
+        // if (req.url == "/" && req.method == "GET") {
+        //     res.writeHead(200, { "content-type": "application/json" });
+        //     res.end(
+        //         JSON.stringify({
+        //             message: "Hello from node js with typescript...",
+        //             path: req.url,
+        //         })
+        //     )
+        // }
         // health route
-        if (req.url == "/api" && req.method == "GET") {
-            res.writeHead(200, { "content-type": "application/json" });
-            res.end(
-                JSON.stringify({
-                    message: "Health status ok",
-                    path: req.url,
-                })
-            )
-        }
+        // if (req.url == "/api" && req.method == "GET") {
+        //     res.writeHead(200, { "content-type": "application/json" });
+        //     res.end(
+        //         JSON.stringify({
+        //             message: "Health status ok",
+        //             path: req.url,
+        //         })
+        //     )
+        // }
         //  post 
-        if (req.url == '/api/users' && req.method == 'POST') {
+        // if (req.url == '/api/users' && req.method == 'POST') {
             // const user = {
             //     id: 1,
             //     name: 'arafin'
